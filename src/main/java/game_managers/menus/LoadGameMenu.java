ae15548f6dfec289ec4f7b2a556ea3863730bcf3
Silviu Hartan <silviuhartan10@gmail.com>
2020-05-22 05:56:27 -0700

    "DBManager Added"

---
+++ src/main/java/game_managers/menus/LoadGameMenu.java
@@ -0,0 +1,175 @@
+package game_managers.menus;
+
+import Constants.Constants;
+import game_managers.db.DBManager;
+import game_managers.logicManagers.GameMainFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LoadGameMenu extends JPanel {
+    Dimension screenSize;
+    int       screenWidth;
+    int       screenHeight;
+
+    private static volatile LoadGameMenu                           loadGameMenu;
+    private static          ReentrantLock                          singletonLock;
+    private                 ArrayList< Font >                      fonts;
+    private                 JButton                                backToMyMenuBtn;
+    private                 JButton                                exitBtn;
+    private                 GameMainFrame                          mainFrameReference;
+    private                 Image                                  animatedGif = null;
+    private                 JLabel                                 textLabel   = null;
+    private                 ArrayList< HashMap< String, Object > > savings;
+
+    public LoadGameMenu(GameMainFrame mainFrameReference) {
+        this.mainFrameReference = mainFrameReference;
+
+        try {
+            initializeVariables();
+        } catch ( IOException | FontFormatException | SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
+
+        initializeLayout();
+        initializeFunctionality();
+    }
+
+    public static game_managers.menus.LoadGameMenu getInstance(GameMainFrame mainFrameReference) {
+        singletonLock = new ReentrantLock();
+        if (loadGameMenu == null) {
+            try {
+                singletonLock.lock();
+                loadGameMenu = new game_managers.menus.LoadGameMenu(
+                        mainFrameReference
+                );
+            } finally {
+                singletonLock.unlock();
+            }
+        }
+        return loadGameMenu;
+    }
+
+    public void initializeVariables() throws IOException, FontFormatException, SQLException, ClassNotFoundException {
+
+        animatedGif = Toolkit.getDefaultToolkit().createImage( Constants.HELP_BACKGROUND_GIF );
+        fonts = new ArrayList< Font >();
+        File            fontSource    = new File( Constants.KENVECTOR_FUTURE_THIN_URL );
+        FileInputStream in            = new FileInputStream( fontSource );
+        Font            titleFont     = Font.createFont( Font.TRUETYPE_FONT, in );
+        Font            titleFont32Pt = titleFont.deriveFont( 64f );
+
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = screenSize.width / 2;
+        screenHeight = screenSize.height / 2;
+        fonts.add( titleFont );
+        fonts.add( titleFont32Pt );
+
+        DBManager                              dbManager = mainFrameReference.getDbManager();
+
+        dbManager.openConnection();
+        int                                    i         = 0;
+        ArrayList< HashMap< String, Object > > container = dbManager.SELECTLastNGameSavings( 4 );
+        while (i < Constants.NR_OF_AVAILABLE_GAME_SAVINGS) {
+            savings.add(
+                    container.get( i )
+            );
+            i++;
+        }
+        dbManager.closeConnection();
+
+    }
+
+    public void initializeLayout() {
+        int backToMenuX = 150;
+        int backToMenuY = screenSize.height - 180;
+        int exitX       = screenSize.width - 350;
+        int exitY       = screenSize.height - 180;
+
+        this.setLayout( null );
+        backToMyMenuBtn = new JButton(
+                Constants.BACK_TO_MAIN_MENU_LABEL,
+                new ImageIcon( Constants.MENU_BUTTON_ICON_URL )
+        );
+        backToMyMenuBtn.setHorizontalTextPosition( SwingConstants.CENTER );
+        backToMyMenuBtn.setOpaque( true );
+        backToMyMenuBtn.setForeground( Color.BLACK );
+        backToMyMenuBtn.setBackground( Color.gray );
+        backToMyMenuBtn.setText( Constants.BACK_TO_MAIN_MENU_LABEL );
+        backToMyMenuBtn.setAlignmentX( Component.CENTER_ALIGNMENT );
+        backToMyMenuBtn.setSize( Constants.MAIN_MENU_BUTTON_WIDTH, Constants.MAIN_MENU_BUTTON_HEIGHT );
+        backToMyMenuBtn.setLocation(
+                backToMenuX,
+                backToMenuY
+        );
+
+
+        exitBtn = new JButton(
+                Constants.EXIT_GAME_LABEL,
+                new ImageIcon( Constants.MENU_BUTTON_ICON_URL )
+        );
+        exitBtn.setHorizontalTextPosition( SwingConstants.CENTER );
+        exitBtn.setOpaque( true );
+        exitBtn.setForeground( Color.BLACK );
+        exitBtn.setBackground( Color.gray );
+        exitBtn.setText( Constants.EXIT_GAME_LABEL );
+        exitBtn.setAlignmentX( Component.CENTER_ALIGNMENT );
+        exitBtn.setSize( Constants.MAIN_MENU_BUTTON_WIDTH, Constants.MAIN_MENU_BUTTON_HEIGHT );
+        exitBtn.setLocation(
+                exitX,
+                exitY
+        );
+    }
+
+    public void initializeFunctionality() {
+        backToMyMenuBtn.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainFrameReference.getPanelSwitcher().show(
+                        mainFrameReference.getMainPanelContainer(),
+                        "MAIN_MENU_PANEL"
+                );
+            }
+        } );
+
+        exitBtn.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit( 0 );
+            }
+        } );
+
+        add( backToMyMenuBtn );
+        add( exitBtn );
+        add( textLabel );
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent( g );
+
+        g.drawImage(
+                animatedGif,
+                0,
+                0,
+                screenSize.width,
+                screenSize.height,
+                this
+        );
+
+        g.setFont( fonts.get( 1 ) );
+        g.setColor( Color.WHITE );
+
+    }
+}
